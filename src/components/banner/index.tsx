5f58687c251d1f8b6829658cc3f30e47e7a66def
PabloMur <polillomurillo@gmail.com>
2023-04-05 12:54:30 -0300

    buttons changes

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -4,10 +4,12 @@ import css from "./styles.module.css";
 const Banner = () => {
   return (
     <div className={css.root}>
-      <h1 className={css.title}>Hola soy Pablo!</h1>
-      <h3>
-        Soy un estudiante de programacion, mas precisamente desarrollo web.
-      </h3>
+      <div className={css.bannerContainer}>
+        <h1 className={css.title}>Hola soy Pablo!</h1>
+        <h3>
+          Soy un estudiante de programacion, mas precisamente desarrollo web.
+        </h3>
+      </div>
     </div>
   );
 };
