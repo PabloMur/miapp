860c43a62a786c76dcd7e61901c573325185cf1b
PabloMur <polillomurillo@gmail.com>
2023-04-12 15:00:16 -0300

    testing some display changes at about me section

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -7,24 +7,23 @@ const AboutContainer = () => {
       <div className={css.root}>
         <div className={css.aboutContainer}>
           <h1 className={css.title}>Acerca de Mi</h1>
-          <p className={css.textContainer}>
-            Me presento como una persona perseverante, alegre y aventurera con
-            una gran pasión por la tecnología. Me caracterizo por ser muy
-            curioso y me encanta entender cómo funcionan las cosas.
-            Constantemente me enriquezco de conocimientos de manera autodidacta
-            y me considero muy autosuficiente e independiente, pero también
-            valoro mucho el trabajo colaborativo y la ayuda mutua en equipo.
-            Creo firmemente que trabajar en equipo nos lleva a resultados no
-            solo mejores, sino también más rápidos. Mi filosofía se basa en el
-            desafío constante y el pensamiento lógico para la resolución de
-            problemas. Además, tengo un lado creativo y tridimensional que me
-            permite no solo solucionar problemas a través del código, sino
-            también diseñar. Siempre estoy dispuesto a aprender y enfrentar
-            nuevos retos para seguir creciendo profesionalmente en el mundo de
-            la programación. ("Este texto fue optimizado con chatGPT.")
-          </p>
         </div>
       </div>
+      <p className={css.textContainer}>
+        Me presento como una persona perseverante, alegre y aventurera con una
+        gran pasión por la tecnología. Me caracterizo por ser muy curioso y me
+        encanta entender cómo funcionan las cosas. Constantemente me enriquezco
+        de conocimientos de manera autodidacta y me considero muy autosuficiente
+        e independiente, pero también valoro mucho el trabajo colaborativo y la
+        ayuda mutua en equipo. Creo firmemente que trabajar en equipo nos lleva
+        a resultados no solo mejores, sino también más rápidos. Mi filosofía se
+        basa en el desafío constante y el pensamiento lógico para la resolución
+        de problemas. Además, tengo un lado creativo y tridimensional que me
+        permite no solo solucionar problemas a través del código, sino también
+        diseñar. Siempre estoy dispuesto a aprender y enfrentar nuevos retos
+        para seguir creciendo profesionalmente en el mundo de la programación.
+        ("Este texto fue optimizado con chatGPT.")
+      </p>
     </>
   );
 };
