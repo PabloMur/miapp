f305c326bb5ef99b9b8c3bd447260a4fbc6daf56
PabloMur <polillomurillo@gmail.com>
2023-04-12 23:57:39 -0300

    nuevos componentes en ui para componer las cards

--- src/components/proyectCard/index.tsx
+++ src/components/proyectCard/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import css from "./styles.module.css";
+import { GitHubButton } from "../../ui/githubLinkButton";
+import DeployButton from "ui/deployLinkButton";
 
-const ProyectCard = () => {
+const ProyectCard = ({
+  image,
+  title,
+  description,
+  githubLink,
+  deployLink,
+}: any) => {
   return (
     <div className={css.root}>
-      <div className={css.imgContainer}></div>
-      <div className={css.infoContainer}></div>
+      <img src={image} alt={title} className={css.imgContainer} />
+      <div className={css.cardBody}>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <div className="card-links">
+          <GitHubButton githubLink={githubLink} />
+          <DeployButton deployLink={deployLink} />
+        </div>
+      </div>
     </div>
   );
 };
