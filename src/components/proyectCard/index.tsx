bc75b180cb1981db8337b13a2038afa2af6c2ce3
PabloMur <polillomurillo@gmail.com>
2023-04-14 19:16:19 -0300

    animacion en proyect card

--- src/components/proyectCard/index.tsx
+++ src/components/proyectCard/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import css from "./styles.module.css";
 import { GitHubButton } from "../../ui/githubLinkButton";
 import DeployButton from "ui/deployLinkButton";
+import { motion } from "framer-motion";
 
 const ProyectCard = ({
   image,
@@ -12,15 +13,21 @@ const ProyectCard = ({
 }: any) => {
   return (
     <div className={css.root}>
-      <img src={image} alt={title} className={css.imgContainer} />
-      <div className={css.cardBody}>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <div className={css.cardLinks}>
-          <GitHubButton githubLink={githubLink} />
-          <DeployButton deployLink={deployLink} />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.7 }}
+      >
+        <img src={image} alt={title} className={css.imgContainer} />
+        <div className={css.cardBody}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <div className={css.cardLinks}>
+            <GitHubButton githubLink={githubLink} />
+            <DeployButton deployLink={deployLink} />
+          </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
