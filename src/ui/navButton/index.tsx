9383476f87c7f8b14a75f0f80f08bf960ef98460
PabloMur <polillomurillo@gmail.com>
2023-04-30 17:02:16 -0300

    ultimando detalles de display

--- src/ui/navButton/index.tsx
+++ src/ui/navButton/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useGoTo } from "../../hooks/uiHooks";
 import css from "./styles.module.css";
 
-function NavButton({ content, route, variant }: any) {
+function NavButton({ content, route }: any) {
   let cont = content;
 
   const goTo = useGoTo();
@@ -10,48 +10,13 @@ function NavButton({ content, route, variant }: any) {
   function HandleRoute() {
     goTo(route);
   }
-
-  if (variant === "root") {
-    return (
-      <>
-        <div className={css.root} onClick={HandleRoute}>
-          {cont}
-        </div>
-      </>
-    );
-  } else if (variant === "alert") {
-    return (
-      <>
-        <div className={css.alert} onClick={HandleRoute}>
-          {cont}
-        </div>
-      </>
-    );
-  } else if (variant === "stack") {
-    return (
-      <>
-        <div className={css.stack} onClick={HandleRoute}>
-          {cont}
-        </div>
-      </>
-    );
-  } else if (variant === "proyects") {
-    return (
-      <>
-        <div className={css.proyects} onClick={HandleRoute}>
-          {cont}
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className={css.contact} onClick={HandleRoute}>
-          {cont}
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className={css.root} onClick={HandleRoute}>
+        {cont}
+      </div>
+    </>
+  );
 }
 
 export { NavButton };
