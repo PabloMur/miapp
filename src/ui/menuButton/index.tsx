1293a77aec0d3e467f8e78868b084f776a2b2c5f
PabloMur <polillomurillo@gmail.com>
2023-05-01 11:52:49 -0300

    banner text movile

--- src/ui/menuButton/index.tsx
+++ src/ui/menuButton/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import css from "./styles.module.css";
 
 const MenuButton = () => {
+  const handleClick = () => {
+    console.log("Click");
+  };
   return (
-    <div className={css.root}>
+    <div className={css.root} onClick={handleClick}>
       <div className={css.line}></div>
       <div className={css.line}></div>
       <div className={css.line}></div>
