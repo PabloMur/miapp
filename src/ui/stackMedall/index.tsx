f6c01b06f0aa1e393a9ab0f8cc3a61d3aeed1578
PabloMur <polillomurillo@gmail.com>
2023-05-04 13:45:00 -0300

    stack medallls

---
+++ src/ui/stackMedall/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import css from "./styles.module.css";
+const StackMedall = ({ name }: any) => {
+  return <div className={css.root}>{name}</div>;
+};
+
+export default StackMedall;
