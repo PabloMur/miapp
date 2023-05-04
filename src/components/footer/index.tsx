06c6a5271cf5c938d0849fffeacd9bdedb086c74
PabloMur <polillomurillo@gmail.com>
2023-05-03 22:56:27 -0300

    footer por el momento completo

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,11 @@ import css from "./styles.module.css";
 const Footer = () => {
   return (
     <>
-      <div className={css.root}></div>
+      <div className={css.root}>
+        <p className={css.text}>
+          Copyright © 2023 Pablo Murillo | Todos los derechos reservados.
+        </p>
+      </div>
     </>
   );
 };
