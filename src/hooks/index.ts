def984d8d65935aee4b2a83dcbdc58f80662a56e
PabloMur <polillomurillo@gmail.com>
2023-05-11 12:46:33 -0300

    sera

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,8 +2,7 @@ import { APISendEmail } from "lib/apiCalls";
 
 export const useSendEmail = () => {
   const emailSender = (email: object) => {
-    const emailSent = APISendEmail(email);
-    return emailSent;
+    return APISendEmail(email);
   };
   return emailSender;
 };
